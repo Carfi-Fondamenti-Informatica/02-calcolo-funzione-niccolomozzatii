d0646156a655fa51a72e642f0222363e1172c5c3
niccolomozzatii <118431871+niccolomozzatii@users.noreply.github.com>
2022-11-17 16:23:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
    float a=0, b=0, x=0, y=0, z=0;
-   cin >> a >> b >> c >> x, >> y;
+   cin >> a >> b >> c >> x >> y;
    if((x<0)&&(y>0)){
       z=(a*x)-(b*y);
       cout << z << endl;
